2cceeb21d20d1dd9ecbea3ac90e49dbccd394a00
adamohern <adam@mechanicalcolor.com>
2016-12-14 14:32:18 -0500

    funnies

--- __init__.py
+++ __init__.py
@@ -167,11 +167,14 @@ class MeshEditorClass():
     def __init__(self, args = None, mesh_edit_flags = []):
         self.args = args
         self.mesh_edit_flags = mesh_edit_flags
+        self.mesh_svc = None
+        self.mesh = None
+        self.mark_mode_checked = None
+        self.mark_mode_unchecked = None
         self.polygon_accessor = None
         self.edge_accessor = None
         self.meshmap_accessor = None
         self.point_accessor = None
-        self.list_of_poly_islands = None
 
     def mesh_edit_action(self):
         return None
@@ -185,7 +188,7 @@ class MeshEditorClass():
     def do_mesh_read(self):
         return self.mesh_edit(True)
 
-    def mesh_edit(self, read_only = False):
+    def mesh_edit(self, read_only=False):
         """Adapted from James O'Hare's excellent code: https://gist.github.com/Farfarer/31148a78f392a831239d9b018b90330c"""
 
         if read_only:
@@ -193,65 +196,76 @@ class MeshEditorClass():
         if not read_only:
             scan_allocate = lx.symbol.f_LAYERSCAN_EDIT
 
-        return_value = None
-
         layer_svc = lx.service.Layer ()
         layer_scan = lx.object.LayerScan (layer_svc.ScanAllocate (scan_allocate))
+
         if not layer_scan.test ():
             return
 
-        mesh_svc = lx.service.Mesh ()
-        mark_mode_checked = mesh_svc.ModeCompose ('user0', None)
-        mark_mode_unchecked = mesh_svc.ModeCompose (None, 'user0')
+        self.mesh_svc = lx.service.Mesh ()
+        self.mark_mode_checked = self.mesh_svc.ModeCompose ('user0', None)
+        self.mark_mode_unchecked = self.mesh_svc.ModeCompose (None, 'user0')
 
         for n in xrange (layer_scan.Count ()):
             if read_only:
-                mesh = lx.object.Mesh (layer_scan.MeshBase(n))
+                self.mesh = lx.object.Mesh (layer_scan.MeshBase(n))
             if not read_only:
-                mesh = lx.object.Mesh (layer_scan.MeshEdit(n))
+                self.mesh = lx.object.Mesh (layer_scan.MeshEdit(n))
 
-            if not mesh.test ():
+            if not self.mesh.test ():
                 continue
 
-            polygon_count = mesh.PolygonCount ()
+            polygon_count = self.mesh.PolygonCount ()
             if polygon_count == 0:
                 continue
 
-            self.polygon_accessor = lx.object.Polygon (mesh.PolygonAccessor ())
+            self.polygon_accessor = lx.object.Polygon (self.mesh.PolygonAccessor ())
             if not self.polygon_accessor.test ():
                 continue
 
-            self.edge_accessor = lx.object.Edge (mesh.EdgeAccessor ())
+            self.edge_accessor = lx.object.Edge (self.mesh.EdgeAccessor ())
             if not self.edge_accessor.test ():
                 continue
 
-            self.point_accessor = lx.object.Point (mesh.PointAccessor ())
+            self.point_accessor = lx.object.Point (self.mesh.PointAccessor ())
             if not self.point_accessor.test ():
                 continue
 
-            self.meshmap_accessor = lx.object.MeshMap (mesh.MeshMapAccessor ())
+            self.meshmap_accessor = lx.object.MeshMap (self.mesh.MeshMapAccessor ())
             if not self.meshmap_accessor.test ():
                 continue
 
-            visClear = SetMarksClass (self.polygon_accessor, mark_mode_unchecked)
-            self.polygon_accessor.Enumerate (mark_mode_checked, visClear, 0)
-
-            visIslands = PolysByIslandClass (self.polygon_accessor, self.point_accessor, mark_mode_checked)
-            self.polygon_accessor.Enumerate (mark_mode_unchecked, visIslands, 0)
-
-            self.list_of_poly_islands = visIslands.islands
+            visClear = SetMarksClass (self.polygon_accessor, self.mark_mode_unchecked)
+            self.polygon_accessor.Enumerate (self.mark_mode_checked, visClear, 0)
 
             if read_only:
-                return_value = self.mesh_read_action()
+                self.mesh_read_action()
             if not read_only:
-                return_value = self.mesh_edit_action()
+                self.mesh_edit_action()
 
             if self.mesh_edit_flags and not read_only:
                 layer_scan.SetMeshChange (n, reduce(ior, self.mesh_edit_flags))
 
         layer_scan.Apply ()
 
-        return return_value
+    def get_polys_by_island(self):
+        visIslands = PolysByIslandClass (self.polygon_accessor, self.point_accessor, self.mark_mode_checked)
+        self.polygon_accessor.Enumerate (self.mark_mode_unchecked, visIslands, 0)
+
+        return visIslands.islands
+
+    def get_polys_by_selected(self):
+        mark_mode = self.mesh_svc.ModeCompose (lx.symbol.sMARK_SELECT, 'hide lock')
+
+        selectedPolygons = set()
+
+        polyCount = self.mesh.PolygonCount ()
+        for p in xrange(polyCount):
+            self.polygon_accessor.SelectByIndex(p)
+            if self.polygon_accessor.TestMarks (mark_mode):
+                selectedPolygons.add (self.polygon_accessor.ID())
+
+        return selectedPolygons
 
 
 class Commander(lxu.command.BasicCommand):
