3323a9945e9873869986326cf69e612c036877e9
adamohern <adam@mechanicalcolor.com>
2017-01-05 19:04:31 -0500

    Funk's category suggestion

--- lxserv/getting_started.py
+++ lxserv/getting_started.py
@@ -6,7 +6,10 @@ class CommandClass(commander.CommanderClass):
     def commander_execute(self, msg, flags):
         kitname = lx.eval('user.value kitty_kit_new_name ?')
         new_kitpath = lx.eval("query platformservice alias ? {%s:}" % kitname)
+
         os.remove(os.path.join(new_kitpath, 'Configs', 'startup.cfg'))
+        os.remove(os.path.join(new_kitpath, 'lxserv', 'getting_started.py'))
+        os.remove(os.path.join(new_kitpath, 'lxserv', 'startup.py'))
 
         modo.dialogs.alert("Kit Initialized", "Your new kit is working. Have fun.")
         lx.eval('user.value kitty_kit_initialize 0')
